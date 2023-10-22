c51643a869342a7446624c8c914992ae26abb8ba
César Patiño <cesarlpb@gmail.com>
2023-10-22 12:36:17 +0200

    Variables

---
+++ variables.java
@@ -0,0 +1,14 @@
+public class variables {
+    public static void main(String[] args){
+       String nombre = "César";
+       int edad = 2023 - 1989;
+       String profesion = "programador";
+
+       System.out.println("Hola, me llamo");
+       System.out.println(nombre);
+       System.out.println("Mi edad es:");
+       System.out.println(edad);
+       System.out.println("Mi profesión es:");
+       System.out.println(profesion);
+    }
+}
