ec6113904ccafa5aa53c8b56e5eda851091d717d
César Patiño <cesarlpb@gmail.com>
2023-10-28 13:08:49 +0200

    Casting

---
+++ casting.java
@@ -0,0 +1,9 @@
+public class casting {
+    public static void main(String[] args) {
+        System.out.println("Casting de byte a double:");
+        byte byte_ = 127;
+        int int_ = byte_;
+        double double_ = byte_;
+        System.out.println("El byte " + byte_ + " es el double " + double_ + " y es el int " + int_);
+    }
+}
