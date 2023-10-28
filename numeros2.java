ac03d04f35840581c55dff5a6bff5f1b927a941b
César Patiño <cesarlpb@gmail.com>
2023-10-28 12:20:12 +0200

    Números máximos en positivo

---
+++ numeros2.java
@@ -0,0 +1,17 @@
+public class numeros2 {
+        public static void main(String[] args) {
+            byte byte_ = 127; 
+            System.out.println("Máx byte: " + byte_);
+            short short_ = 32767;
+            System.out.println("Máx short: " + short_);
+            int int_ = 2147483647;
+            System.out.println("Máx int: " + int_);
+            long long_ = 9223372036854775807L;
+            System.out.println("Máx long: " + long_);
+            float float_ = 19.999999f;
+            System.out.println("Máx float: " + float_);
+            double double_ = 0.9999999999999999;
+            System.out.println("Máx double: " + double_);
+            // Ejercicio: Hacer lo mismo con negativos.
+        }    
+}
