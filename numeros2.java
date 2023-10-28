c04e0aa8dab4249350db9709bc2039c0d178e364
César Patiño <cesarlpb@gmail.com>
2023-10-28 12:32:06 +0200

    Notación científica

--- numeros2.java
+++ numeros2.java
@@ -13,5 +13,19 @@ public class numeros2 {
             double double_ = 0.9999999999999999;
             System.out.println("Máx double: " + double_);
             // Ejercicio: Hacer lo mismo con negativos.
+            // Notación científica
+            System.out.println("Notación Científica");
+            
+            System.out.println("Número grande:");
+            int millon = 1000000;
+            float millon_ = 1e6f; // e6 -> 6 ceros a la derecha
+            System.out.println(millon);
+            System.out.println(millon_);
+            
+            System.out.println("Número pequeño:");
+            float milesima = 0.001f;
+            float milesima_ = 1e-3f; // e-3 -> 3 ceros a la izquierda
+            System.out.println(milesima);
+            System.out.println(milesima_);
         }    
 }
