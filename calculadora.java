1690999ef30a92c76d381a7096bdc7da8cfe93c9
César Patiño <cesarlpb@gmail.com>
2023-10-29 13:23:38 +0100

    Añadimos if else -> evitar error división por cero

--- calculadora.java
+++ calculadora.java
@@ -13,7 +13,12 @@ public class calculadora {
     System.out.println("Suma: " + (num1 + num2));
     System.out.println("Resta: " + (num1 - num2));
     System.out.println("Producto: " + (num1 * num2));
-    System.out.println("División: " + (num1 / num2));
+    // Añadimos if else para evitar error de división por cero
+    if(num2 == 0){
+      System.out.println("No se puede dividir por cero");
+    } else {
+      System.out.println("División: " + (num1 / num2));
+    }
     scanner.close();
   }
 }
