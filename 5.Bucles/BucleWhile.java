c30b367bab260bc8c021a23986ef0c0f8adba652
César P. <cesarlpb@gmail.com>
2023-11-04 13:41:37 +0100

    edit

--- 5.Bucles/BucleWhile.java
+++ 5.Bucles/BucleWhile.java
@@ -1,6 +1,6 @@
 public class BucleWhile {
     public static void main(String[] args) {
-        int i = 1;
+        int i = 0;
         while (i <= 10) {
             System.out.print(i + " ");
             i++;
