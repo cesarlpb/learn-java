3dd941c1203bb5cea75e116d5225a271f7946410
César P. <cesarlpb@gmail.com>
2023-11-04 12:59:55 +0100

    Bucle Switch

---
+++ 5.Bucles/BucleSwitch.java
@@ -0,0 +1,30 @@
+public class BucleSwitch {
+    public static void main(String[] args) {
+    int dia = 6;
+    switch (dia) {
+    case 1:
+        System.out.println("Lunes");
+        break;
+    case 2:
+        System.out.println("Martes");
+        break;
+    case 3:
+        System.out.println("Miércoles");
+        break;
+    case 4:
+        System.out.println("Jueves");
+        break;
+    case 5:
+        System.out.println("Viernes");
+        break;
+    case 6:
+        System.out.println("Sábado");
+        break;
+    case 7:
+        System.out.println("Domingo");
+        break;
+    default:
+        System.out.println("Ese valor no es un día de la semana");
+        }
+    }
+}
