44c684dd858fda0857c3eb61dad772bcfed55f33
César P. <cesarlpb@gmail.com>
2023-11-04 12:41:40 +0100

    Escribir en archivo TXT

---
+++ 6.Ficheros/escribirTxt.java
@@ -0,0 +1,23 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class escribirTxt {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce una línea de texto para guardar en el log:");
+        String linea = scanner.nextLine();
+
+        try {
+            FileWriter writer = new FileWriter("log.txt", true); // Abre el archivo en modo de anexar
+            writer.write(linea + "\n"); // Escribe la línea en el archivo
+            writer.close(); // Cierra el archivo
+
+            System.out.println("Línea guardada en el 'log.txt' correctamente.");
+        } catch (IOException e) {
+            System.err.println("Error al guardar en el log: " + e.getMessage());
+        }
+        scanner.close();
+    }
+}
