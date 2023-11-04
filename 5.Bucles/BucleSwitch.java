9da5f94062cb9cb27bb896ac432764319fd2b33a
César P. <cesarlpb@gmail.com>
2023-11-04 13:35:03 +0100

    indentación

--- 5.Bucles/BucleSwitch.java
+++ 5.Bucles/BucleSwitch.java
@@ -6,30 +6,30 @@ public class BucleSwitch {
     System.out.println("Introduce un día de la semana (1 a 7):");
     int dia = scanner.nextInt();
     switch (dia) {
-    case 1:
-        System.out.println("Lunes");
-        break;
-    case 2:
-        System.out.println("Martes");
-        break;
-    case 3:
-        System.out.println("Miércoles");
-        break;
-    case 4:
-        System.out.println("Jueves");
-        break;
-    case 5:
-        System.out.println("Viernes");
-        break;
-    case 6:
-        System.out.println("Sábado");
-        break;
-    case 7:
-        System.out.println("Domingo");
-        break;
-    default:
-        System.out.println("Ese valor no es un día de la semana");
-        }
+        case 1:
+            System.out.println("Lunes");
+            break;
+        case 2:
+            System.out.println("Martes");
+            break;
+        case 3:
+            System.out.println("Miércoles");
+            break;
+        case 4:
+            System.out.println("Jueves");
+            break;
+        case 5:
+            System.out.println("Viernes");
+            break;
+        case 6:
+            System.out.println("Sábado");
+            break;
+        case 7:
+            System.out.println("Domingo");
+            break;
+        default:
+            System.out.println("Ese valor no es un día de la semana");
+    }
     scanner.close();
     }
 }
