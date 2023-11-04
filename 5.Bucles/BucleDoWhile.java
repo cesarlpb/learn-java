afd6070737bb4a263185f4ac198b6919b23434f5
César P. <cesarlpb@gmail.com>
2023-11-04 13:47:27 +0100

    Bucle do while

---
+++ 5.Bucles/BucleDoWhile.java
@@ -0,0 +1,9 @@
+public class BucleDoWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.print(i + " ");
+            i++;
+        } while (i <= 10);
+    }    
+}
